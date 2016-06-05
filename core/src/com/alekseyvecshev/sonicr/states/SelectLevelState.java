26bf207c7298d9b57953542b8823ed2ecfb8301c
ultimatum424 <ultimatum424@yandex.ru>
2016-06-06 00:47:16 +0300

    Добавлено меню инфо

--- core/src/com/alekseyvecshev/sonicr/states/SelectLevelState.java
+++ core/src/com/alekseyvecshev/sonicr/states/SelectLevelState.java
@@ -16,8 +16,9 @@ import com.badlogic.gdx.utils.Array;
  * Created by Алексей on 14.05.2016.
  */
 public class SelectLevelState extends State{
-    Texture bk;
+    Texture bg;
     Texture black;
+    Texture help;
     Array<Vector2> posBlack;
     Rectangle lvl10;
     Rectangle lvl11;
@@ -25,10 +26,13 @@ public class SelectLevelState extends State{
     Rectangle lvl21;
     Rectangle lvl30;
     Rectangle lvl31;
+    boolean isHelp;
     public SelectLevelState(GameStateManager gsm) {
         super(gsm);
-        bk = new Texture("SelectLevel/bk.png");
+        bg = new Texture("SelectLevel/bg.png");
         black = new Texture("SelectLevel/black.png");
+        help = new Texture("SelectLevel/info.png");
+
 
         lvl10 = new Rectangle(25 , 460, 305, 240);
         lvl11 = new Rectangle(25 , 160, 305, 240);
@@ -36,7 +40,7 @@ public class SelectLevelState extends State{
         lvl21 = new Rectangle(480 , 160, 305, 240);
         lvl30 = new Rectangle(930 , 460, 305, 240);
         lvl31 = new Rectangle(930 , 160, 305, 240);
-
+        isHelp = false;
         posBlack = new Array<Vector2>();
         posBlack.addAll( new Vector2(25, 160),new Vector2(480, 460), new Vector2(480, 160), new Vector2(930, 460));
         camera.setToOrtho(false, SonicRGame.WIDTH, SonicRGame.HEIGHT);
@@ -47,36 +51,45 @@ public class SelectLevelState extends State{
         Preferences prefs = Gdx.app.getPreferences("Level");
         Preferences prefs2 = Gdx.app.getPreferences("LevelOpen");
         int numberLevel = prefs2.getInteger("level");
-         //prefs2.putInteger("level", 3);
-         //prefs2.flush();
         if (Gdx.input.justTouched()) {
+            System.out.println(isHelp);
             Vector3 touchPos = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
             camera.unproject(touchPos);
             System.out.println(touchPos);
-            if (lvl10.contains(touchPos.x, touchPos.y) && numberLevel >= 0){
-                prefs.putInteger("number", 1);
-                prefs.flush();
-                gsm.set(new PlayState(gsm));
+            if (!isHelp) {
+                if (lvl10.contains(touchPos.x, touchPos.y) && numberLevel >= 0) {
+                    prefs.putInteger("number", 1);
+                    prefs.flush();
+                    gsm.set(new PlayState(gsm));
+                }
+                if (lvl20.contains(touchPos.x, touchPos.y) && numberLevel >= 2) {
+                    prefs.putInteger("number", 2);
+                    prefs.flush();
+                    gsm.set(new PlayState(gsm));
+                }
+                if (lvl30.contains(touchPos.x, touchPos.y) && numberLevel >= 4) {
+                    prefs.putInteger("number", 3);
+                    prefs.flush();
+                    gsm.set(new BossChaosState(gsm));
+                }
+                if (lvl11.contains(touchPos.x, touchPos.y) && numberLevel >= 1) {
+                    prefs.putInteger("number", 1);
+                    prefs.flush();
+                    gsm.set(new BossRobotState(gsm));
+                }
+                if (lvl21.contains(touchPos.x, touchPos.y) && numberLevel >= 3) {
+                    prefs.putInteger("number", 2);
+                    prefs.flush();
+                    gsm.set(new BossShadowState(gsm));
+                }
+                if (lvl31.contains(touchPos.x, touchPos.y)) {
+                    if (!isHelp) {
+                        isHelp = true;
+                    }
+                }
             }
-            if (lvl20.contains(touchPos.x, touchPos.y) && numberLevel >= 2){
-                prefs.putInteger("number", 2);
-                prefs.flush();
-                gsm.set(new PlayState(gsm));
-            }
-            if (lvl30.contains(touchPos.x, touchPos.y)&& numberLevel >= 4) {
-                prefs.putInteger("number", 3);
-               prefs.flush();
-                gsm.set(new BossChaosState(gsm));
-            }
-            if (lvl11.contains(touchPos.x, touchPos.y)&& numberLevel >= 1) {
-                prefs.putInteger("number", 1);
-                prefs.flush();
-                gsm.set(new BossRobotState(gsm));
-            }
-            if (lvl21.contains(touchPos.x, touchPos.y)&& numberLevel >= 3) {
-                prefs.putInteger("number", 2);
-                prefs.flush();
-                gsm.set(new BossShadowState(gsm));
+            else {
+                isHelp = false;
             }
         }
     }
@@ -99,8 +112,11 @@ public class SelectLevelState extends State{
     public void render(SpriteBatch sb) {
         sb.begin();
         sb.setProjectionMatrix(camera.combined);
-        sb.draw(bk, 0 ,0);
+        sb.draw(bg, 0 ,0);
         renderBlack(sb);
+        if (isHelp){
+            sb.draw(help, 0 , 0);
+        }
         sb.end();
     }
 
